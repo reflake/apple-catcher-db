a42fad3fca45f52e99f76a6bd3585ab0ddc34da8
reflake <reflakeykt@gmail.com>
2024-01-12 14:33:31 +0600

    Implement Leaderboard controller

---
+++ src/Controller/LeaderboardController.cs
@@ -0,0 +1,15 @@
+﻿using Database;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Leaderboard
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class LeaderboardController : BaseLeaderboardController<LeaderboardEntry>
+	{
+		public LeaderboardController(AppDbContext<LeaderboardEntry> dbContext) : base(dbContext)
+		{
+		}
+	}
+}
---
+++ src/Entities/LeaderboardEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+	[PrimaryKey("Id")]
+	public class LeaderboardEntry : ILeaderboardEntry
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		public int Scores { get; set; }
+		public int UserId { get; set;  }
+		public DateTime DateTime { get; set; }
+	}
+}
