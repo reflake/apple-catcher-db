75a2d84feee8b265d97a7fe642ed1a9bc9f76142
reflake <reflakeykt@gmail.com>
2024-01-29 23:53:19 +0600

    feat: declare User entity

---
+++ src/Entities/IUserEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities
+{
+	public interface IUserEntry
+	{
+		int Id { get; set; }
+		string Username { get; set; }
+		string Password { get; set; }
+	}
+}
---
+++ src/Entities/UserEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+	[PrimaryKey("Id")]
+	public class UserEntry : IUserEntry
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		public string Username { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+		public DateTime CreationDate { get; set; }
+	}
+}
