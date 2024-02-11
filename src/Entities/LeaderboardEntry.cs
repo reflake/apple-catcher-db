beb6caf5282293a2e7d91391a9cc17f85fb23045
reflake <reflakeykt@gmail.com>
2024-02-11 14:46:10 +0600

    time field

--- src/Entities/LeaderboardEntry.cs
+++ src/Entities/LeaderboardEntry.cs
@@ -10,6 +10,7 @@ namespace Entities
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
 		public int Scores { get; set; }
+		public int Time { get; set; }
 		public DateTime DateTime { get; set; }
 		public string Nickname { get; set; } = string.Empty;
 
